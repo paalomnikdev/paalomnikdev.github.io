17259783247e1e1998ed56c66ce5ab3b0719820b
Ivan Cherniy <paalomnikdev@users.noreply.github.com>
2021-01-23 00:23:20 +0200

    print hidden

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,6 +2,20 @@ if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').match
     document.body.classList.add("dark");
 }
 
-$(document).ready(function () {
-    alert(123);
+jQuery('#print').on('click', function () {
+    const doc = new jspdf.jsPDF();
+    var elementHandler = {
+        '.no-print': function (element, renderer) {
+            return true;
+        }
+    };
+    var source = window.document.getElementsByTagName("body")[0];
+    doc.fromHTML(
+        source,
+        15,
+        15,
+        {
+            'width': 180,'elementHandlers': elementHandler
+        });
+    doc.save('Ivan_Chornyi.pdf');
 });
