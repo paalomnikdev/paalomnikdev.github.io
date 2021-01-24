7bf36adbc2428ccbc52662120c066cf99745226e
Ivan Cherniy <paalomnikdev@users.noreply.github.com>
2021-01-24 20:03:52 +0200

    UI fixes

--- _site/assets/js/index.js
+++ _site/assets/js/index.js
@@ -1,21 +1,3 @@
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
     document.body.classList.add("dark");
 }
-
-jQuery('#print').on('click', function () {
-    const doc = new jspdf.jsPDF();
-    var elementHandler = {
-        '.no-print': function (element, renderer) {
-            return true;
-        }
-    };
-    var source = window.document.getElementsByTagName("body")[0];
-    doc.fromHTML(
-        source,
-        15,
-        15,
-        {
-            'width': 180,'elementHandlers': elementHandler
-        });
-    doc.save('Ivan_Chornyi.pdf');
-});
