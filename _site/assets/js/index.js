623867dcb5044dd3dcf3094f97fee2d6bfaf05c5
Ivan Cherniy <paalomnikdev@users.noreply.github.com>
2021-01-24 21:11:43 +0200

    day/night mode

--- _site/assets/js/index.js
+++ _site/assets/js/index.js
@@ -1,3 +1,27 @@
+const $body = jQuery('body');
+const $dayNightSwitch = jQuery('#day-night-checkbox');
+
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    document.body.classList.add("dark");
+    $body.addClass('dark');
+    $dayNightSwitch.prop('checked', false);
+} else {
+    $body.removeClass('dark');
+    $dayNightSwitch.prop('checked', true);
 }
+
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
+    if (e.matches) {
+        $body.addClass('dark');
+    } else {
+        $body.removeClass('dark');
+    }
+    $dayNightSwitch.prop('checked', !e.matches);
+});
+
+$dayNightSwitch.on('change', function(){
+    if (this.checked) {
+        jQuery('body').removeClass('dark');
+    } else {
+        jQuery('body').addClass('dark');
+    }
+});
