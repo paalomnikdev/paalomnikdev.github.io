7117d44112306bc2c624e6c15e11459f2840ab1b
Ivan Cherniy <paalomnikdev@users.noreply.github.com>
2021-01-24 21:21:46 +0200

    day/night cleanup

--- _site/assets/js/index.js
+++ _site/assets/js/index.js
@@ -1,27 +1,24 @@
 const $body = jQuery('body');
 const $dayNightSwitch = jQuery('#day-night-checkbox');
 
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    $body.addClass('dark');
-    $dayNightSwitch.prop('checked', false);
-} else {
-    $body.removeClass('dark');
-    $dayNightSwitch.prop('checked', true);
-}
-
-window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
-    if (e.matches) {
+const setNight = (night = true) => {
+    if (night) {
         $body.addClass('dark');
     } else {
         $body.removeClass('dark');
     }
-    $dayNightSwitch.prop('checked', !e.matches);
+
+    $dayNightSwitch.prop('checked', !night);
+};
+
+setNight(
+    window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
+);
+
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
+    setNight(e.matches);
 });
 
 $dayNightSwitch.on('change', function(){
-    if (this.checked) {
-        jQuery('body').removeClass('dark');
-    } else {
-        jQuery('body').addClass('dark');
-    }
+    setNight(!this.checked);
 });
